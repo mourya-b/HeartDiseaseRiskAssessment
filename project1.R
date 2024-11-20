14cf740323926fbf019435546e3df4a205ca15bd
RichardRepos <richard.schwartzkopf@ru.nl>
2024-11-20 10:13:02 +0100

    Update project1.R

--- project1.R
+++ project1.R
@@ -1,21 +1,22 @@
-library( gRain )
+library(gRain)
 library(dagitty)
 library(ucimlrepo)
 library(fastDummies)
 library(CCP)
+
+#cp, restecg, thal
+
+
 iris_by_name <- fetch_ucirepo(name = "Heart Disease")
 data = iris_by_name[["data"]][["original"]]
 
 df <- as.data.frame(data)
 # Specify data types
-#categorical 
 df$sex <- factor(df$sex, 
                 levels = c(0,1), 
                 labels = c("female", "male"))
 ##maybe ordinal
-df$cp <- factor(df$cp, 
-               levels = c(1, 2, 3, 4), 
-               labels = c("typical angina", "atypical angina", "non-anginal pain", "asymptomatic"))
+df$cp <- factor(df$cp, levels = c(1, 2, 3, 4), labels = c("typical angina", "atypical angina", "non-anginal pain", "asymptomatic"))
 
 df$fbs <- factor(df$fbs, 
                  levels = c(0,1), 
@@ -63,21 +64,86 @@ slope [pos="0.5,0.55"]
 ca [pos="0.4,0.2"]           
 thal [pos="0.9,0.4"]         
 num [pos="0.7,0.7"]          
-age -> { cp trestbps chol fbs thalach }
+age -> { cp trestbps chol fbs thalach ca}
 sex -> { trestbps chol fbs restecg thalach exang}
-cp -> { num }
 trestbps -> { cp restecg exang }
-chol -> { restecg cp thalach trestbps }
+chol -> { restecg cp thalach trestbps num}
 fbs -> { chol slope exang oldpeak trestbps}
-restecg -> { oldpeak thal num}
-slope -> { cp exang oldpeak num }
-ca -> { exang oldpeak thal num }
+slope -> { cp exang oldpeak }
+ca -> { exang oldpeak }
 thal -> { exang num }
 thalach -> {restecg oldpeak}
+exang -> {cp}
+num -> {ca cp restecg thalach}
 }
 ')
 plot(g)
 
+addEdge <- function(g, from, to) {
+  # Extract the current graph definition
+  graph_string <- as.character(g)
+  updated_graph_string <- sub("\\}", paste0(from, " -> ", to, "\n}"), graph_string)
+  g = dagitty(updated_graph_string)
+  return(g)
+}
+
+#thalach -> slope
+g <- addEdge( g, "thalach", "slope")
+g <- graphLayout(g)
+impliedConditionalIndependencies(g)
+r <- localTests(g,df, type='cis.pillai')
+plotLocalTestResults(r)
+print(mean(abs(r$estimate)))
+
+if (isAcyclic(g)) {
+  cat("The graph is acyclic (DAG). No cycles detected.\n")
+} else {
+  cat("The graph contains cycles. It is not a valid DAG.\n")
+}
+
+#sex -> thal
+g <- addEdge(g, "sex", "thal")
+g <- graphLayout(g)
+impliedConditionalIndependencies(g)
+r <- localTests(g,df, type='cis.pillai')
+plotLocalTestResults(r)
+print(mean(abs(r$estimate)))
+
+if (isAcyclic(g)) {
+  cat("The graph is acyclic (DAG). No cycles detected.\n")
+} else {
+  cat("The graph contains cycles. It is not a valid DAG.\n")
+}
+
+# num -> oldpeak
+g <- addEdge(g, "num", "oldpeak")
+g <- graphLayout(g)
+impliedConditionalIndependencies(g)
+r <- localTests(g,df, type='cis.pillai')
+plotLocalTestResults(r)
+print(mean(abs(r$estimate)))
+
+if (isAcyclic(g)) {
+  cat("The graph is acyclic (DAG). No cycles detected.\n")
+} else {
+  cat("The graph contains cycles. It is not a valid DAG.\n")
+}
+
+#num -> exang
+g <- addEdge(g, "num", "exang")
+g <- graphLayout(g)
+impliedConditionalIndependencies(g)
+r <- localTests(g,df, type='cis.pillai')
+plotLocalTestResults(r)
+print(mean(abs(r$estimate)))
+
+if (isAcyclic(g)) {
+  cat("The graph is acyclic (DAG). No cycles detected.\n")
+} else {
+  cat("The graph contains cycles. It is not a valid DAG.\n")
+}
+
+##
 r <- c()
 for( n in names(g) ){
   for( p in dagitty::parents(g,n) ){
@@ -94,63 +160,23 @@ g_with_coefficients <- dagitty( r.dagitty )
 coordinates( g_with_coefficients ) <- coordinates( g )
 plot( g_with_coefficients, show.coefficients=TRUE )
 
-filtered_r <- r[r$p <= 0.05 & r$cor > 0.1, ]
+filtered_r <- r[r$p <= 0.05 & r$cor > 0.01,]
+r_del <- r[r$p > 0.05 | r$cor <= 0.01,]
+
+#filtered_r <- r[r$p <= 0.05,]
+#r_del <- r[r$p > 0.05,]
+
+filtered_r <- r[r$cor > 0.1,]
+r_del <- r[r$cor <= 0.1,]
+
 length(filtered_r$X)
-
-g <- dagitty('
-dag {
-bb="0,0,1,1"
-age [pos="0.7,0.1"]         
-cp [pos="0.6,0.55"]          
-trestbps [pos="0.4,0.8"]     
-chol [pos="0.2,0.2"]         
-fbs [pos="0.35,0.4"]        
-restecg [pos="0.35,0.3"]     
-exang [pos="0.75,0.25"]      
-oldpeak [pos="0.5,0.4"]     
-slope [pos="0.5,0.55"]       
-ca [pos="0.4,0.1"]           
-thal [pos="0.9,0.4"]         
-num [pos="0.5,0.7"]          
-age -> { chol fbs trestbps }
-slope -> { cp exang num oldpeak }
-thal -> { exang num}
-ca -> { num oldpeak thal }
-cp -> { num }
-chol -> {restecg}
-fbs -> { trestbps }
-}
-')
-
-g <- dagitty('
-dag {
-bb="0,0,1,1"
-age [pos="0.7,0.1"]         
-cp [pos="0.4,0.55"]          
-trestbps [pos="0.4,0.8"]     
-chol [pos="0.2,0.2"]         
-fbs [pos="0.35,0.4"]        
-restecg [pos="0.35,0.3"]     
-exang [pos="0.75,0.25"]      
-oldpeak [pos="0.5,0.7"]       
-slope [pos="0.5,0.55"]       
-ca [pos="0.8,0.65"]           
-thal [pos="0.9,0.4"]         
-num [pos="0.5,0.4"]        
-age -> { chol fbs trestbps }
-slope -> { cp exang num oldpeak }
-thal -> { exang num}
-ca -> { num oldpeak thal }
-cp -> { num }
-chol -> {restecg}
-fbs -> { trestbps }
-}
-')
+length(r_del$X)
 
 filtered_r.dagitty <- paste(filtered_r$X, filtered_r$A, filtered_r$Y, "[beta=",signif(filtered_r$cor,2),"] ", collapse="\n")
 g_with_coefficients <- dagitty( filtered_r.dagitty )
 coordinates( g_with_coefficients ) <- coordinates( g )
-plot( g_with_coefficients, show.coefficients=TRUE )
+plot(g_with_coefficients, show.coefficients=TRUE )
 
+hist(df$age)
 
 
